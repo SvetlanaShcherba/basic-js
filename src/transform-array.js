d9f76943376f6ba23a2bf3432a076996fc5372ed
shcherba sviatlana <shcherba.svetlana4@gmail.com>
2023-04-13 09:35:46 +0300

    feat: fix the solution of the task

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,40 +13,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(arr) {
-  throw new NotImplementedError('Not implemented');
-  let newArr=[...arr];
-    if (Array.isArray(arr)) {
+function transform(arr) {  
+    if (!Array.isArray(arr)) {
+      throw new Error ("'arr' parameter must be an instance of the Array!");  
+    }
+    else  {
+      let newArr=[...arr];
       for (let i=0; i<newArr.length; i++) {
         if (newArr[i]==='--discard-next' && i!==newArr.length-1 ) {
-          newArr.splice(i, 2);
+          newArr.splice(i, 2, 0);
+          console.log(newArr);
         }
-        else if (newArr[i]==='--discard-next' && i==newArr.length-1 ) {
+        if (newArr[i]==='--discard-next' && i==newArr.length-1 ) {
           newArr.splice(i, 1);
+          console.log(newArr);
         }   
-        else if (newArr[i]==="--discard-prev" && i!==0 ) {
+        if (newArr[i]==="--discard-prev" && i!==0 && (i-1)!==0 ) {
           newArr.splice(i-1, 2);
         }
-        else if (newArr[i]==="--discard-prev" && i==0 ) {
+        if (newArr[i]==="--discard-prev" && i!==0 && (i-1)==0 ) {
+          newArr.splice(i, 0);
+        }
+        if (newArr[i]==="--discard-prev" && i==0 ) {
           newArr.splice(i, 1);
         }
-        else if (newArr[i]==="--double-next" && i!==newArr.length-1) {
+        if (newArr[i]==="--double-next" && i!==newArr.length-1) {
           newArr.splice(i, 1, newArr[i+1] );
         }
-        else if (newArr[i]==="--double-next" && i==newArr.length-1) {
+        if (newArr[i]==="--double-next" && i==newArr.length-1) {
           newArr.splice(i, 1);
         }
-        else if (newArr[i]==="--double-prev" && i!==0) {
+        if (newArr[i]==="--double-prev" && i!==0) {
           newArr.splice(i, 1, newArr[i-1]);
         }
-        else if (newArr[i]==="--double-prev" && i==0) {
+        if (newArr[i]==="--double-prev" && i==0) {
           newArr.splice(i, 1);
         }
       }
-    }
-    else 
-      alert("'arr' parameter must be an instance of the Array!"); 
-  return newArr;     
+      newArr=newArr.filter ((elem)=> elem!==0);
+      return newArr;
+    }          
 }
 
 module.exports = {
